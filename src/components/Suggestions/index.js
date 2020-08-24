d694df0f54c2ce1deb57d9aea7643b2859334168
Marcos Ferreira <marcosferreira.developer@gmail.com>
2020-08-24 08:26:05 -0300

    feature final Suggestions

--- src/components/Suggestions/index.js
+++ src/components/Suggestions/index.js
@@ -1,17 +1,65 @@
 import React from 'react'
 
 import img1 from '../../assets/images/01.png';
+import img2 from '../../assets/images/02.png';
+import img3 from '../../assets/images/03.png';
+import img4 from '../../assets/images/04.png';
+import img5 from '../../assets/images/05.png';
+import img6 from '../../assets/images/06.png';
+import img7 from '../../assets/images/07.png';
 
 import { Container, Option, Img, Label } from './styles';
 
-export default function Suggestions() {
+const items = [
+  {
+    key: String(Math.random()),
+    img: img1,
+    label: 'Doações'
+  },
+  {
+    key: String(Math.random()),
+    img: img2,
+    label: 'Doações'
+  },
+  {
+    key: String(Math.random()),
+    img: img3,
+    label: 'Doações'
+  },
+  {
+    key: String(Math.random()),
+    img: img4,
+    label: 'Doações'
+  },
+  ,
+  {
+    key: String(Math.random()),
+    img: img5,
+    label: 'Doações'
+  },
+  {
+    key: String(Math.random()),
+    img: img6,
+    label: 'Doações'
+  },
+  {
+    key: String(Math.random()),
+    img: img7,
+    label: 'Doações'
+  },
+];
+
+function Suggestions() {
   return (
     <Container>
-      <Option>
-        <Img source={img1} />
-        <Label>Doações</Label>
-      </Option>
+      {items.map((item) => (
+        <Option key={item.key}>
+          <Img source={item.img}></Img>
+          <Label>{item.label}</Label>
+        </Option>
+      ))}
     </Container>
   );
 }
 
+export default Suggestions;
--- src/components/Suggestions/styles.js
+++ src/components/Suggestions/styles.js
@@ -11,9 +11,18 @@ export const Container = styled.ScrollView.attrs(() => ({
   height: 130px;
 `;
 
-export const Option = styled.TouchableOpacity``;
+export const Option = styled.TouchableOpacity`
+  width: 80px;
+  margin-right: 16px;
+  align-items: center;
+`;
 
 export const Img = styled.Image``;
 
-export const Label = styled.Text``;
+export const Label = styled.Text`
+  color: #fff;
+  font-size: 14px;
+  font-weight:bold;
+  margin-top: 8px;
+`;
 
