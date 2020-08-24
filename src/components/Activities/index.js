ecfeed488f6adc03ec8d801c4abc43eb3258230a
Marcos Ferreira <marcosferreira.developer@gmail.com>
2020-08-24 14:34:28 -0300

    cards section completed

--- src/components/Activities/index.js
+++ src/components/Activities/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Feather } from '@expo/vector-icons';
+import { Feather, MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
 
 import avatar from '../../assets/images/avatar.png';
 
-import { Container, Header, Title, Card, CardHeader, Avatar, Description, Bold, CardBody, UserName, CardFooter, Details, Value, Divider, Date } from './styles';
+import { Container, Header, Title, Card, CardHeader, Avatar, Description, Bold, CardBody, UserName, CardFooter, Details, Value, Divider, Date, Actions, Option, OptionLabel } from './styles';
 
 function Activities() {
   return (
@@ -33,6 +33,18 @@ function Activities() {
             <Feather name="lock" color="#fff" size={14} />
             <Date>há 2 anos</Date>
           </Details>
+
+          <Actions>
+            <Option>
+              <MaterialCommunityIcons name="comment-outline" size={14} color="#fff" />
+              <OptionLabel>0</OptionLabel>
+            </Option>
+
+            <Option>
+              <AntDesign name="hearto" size={14} color="#fff" />
+              <OptionLabel>0</OptionLabel>
+            </Option>
+          </Actions>
         </CardFooter>
       </Card>
     </Container>
